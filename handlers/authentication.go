0ea3d232d1749cc3b92d3a5f3df78e0def887cdc
Nancy Butler <butlern0513@gmail.com>
2019-07-27 18:21:01 -0700

    added protected route

--- handlers/authentication.go
+++ handlers/authentication.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -13,22 +14,54 @@ var signingKey = []byte("supersecret")
 
 // GetToken creates a 24-hour JWT with the given name and admin status as claims
 func GetToken(isAdmin bool, name string) (tokenString string, err error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	claims := token.Claims.(jwt.MapClaims)
-
-	/* Set token claims */
-	claims["admin"] = isAdmin
-	claims["name"] = name
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"admin": isAdmin,
+		"name":  name,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	})
 
 	/* Sign the token with secret */
 	tokenString, err = token.SignedString(signingKey)
 	return tokenString, nil
 }
 
+// check that the JWT is using the signing method expected
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	}
+	return signingKey, nil
+}
+
+// ValidateToken takes in a token, parses, returns bool
+func ValidateToken(tokenString string) (isValid bool, err error) {
+	if token, err := jwt.Parse(tokenString, keyFunc); err != nil {
+		return false, err
+	} else {
+		if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			return true, nil
+		}
+		return false, nil
+	}
+
+}
+
+// NeedsToken middleware checks validity of tokenString in req header then continues or sends 403 forbidden
+func NeedsToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("authorization")
+		isValid, err := ValidateToken(tokenString)
+		if err != nil || !isValid {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 // LogIn gets name and admin status from headers, create JWT, return JWT as JSON
 func LogIn(w http.ResponseWriter, r *http.Request) {
+	// TODO: add some kind of authentication here
 	name := r.Header.Get("name")
 	isAdmin := r.Header.Get("isAdmin") == "true"
 	tokenString, err := GetToken(isAdmin, name)
--- router.go
+++ router.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/mantis_toboggan_md/go_test/handlers"
 )
 
-// GorrilaRouter creates mux router to manage "handlers"
+/* GorrilaRouter creates mux router to manage "handlers"
+public routes:
+	GET "/" - index
+	POST "/login" - login, get JWT
+private routes:
+	GET "/other" - placeholder page
+*/
 func GorillaRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handlers.PublicServe)
 	r.HandleFunc("/login", handlers.LogIn).Methods("POST")
-	r.HandleFunc("/other", handlers.OtherServe)
+	// middleware is ugly but if more is needed, alic package may be used
+	r.Handle("/other", handlers.NeedsToken(http.HandlerFunc(handlers.OtherServe)))
 	return r
 }
